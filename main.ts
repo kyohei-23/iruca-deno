7384bad821ad57254252a73a6b9a8374ebbbf613
KyoheiOrg <tgifz5jmr@gmail.com>
2023-03-03 10:46:29 +0900

    reduce unuse var

--- main.ts
+++ main.ts
@@ -8,6 +8,5 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 serve(async (req)=> {
   const body:JSON = await req.json()
   const body_string = JSON.stringify(body)
-  const res = await fetch('sas')
   return new Response(body_string)
 })
