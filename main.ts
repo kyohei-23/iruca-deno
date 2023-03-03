c629fdcf0c509f73f02aef0936efd0c1af58b4d3
KyoheiOrg <tgifz5jmr@gmail.com>
2023-03-03 10:42:17 +0900

    catch body of request

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-serve((req)=> new Response('Hello World!'))
+const ROOM_ID = Deno.env.get('ROOM_ID')
+const API_KEY = Deno.env.get('API_KEY')
+
+
+
+serve(async (req)=> {
+  const body:JSON = await req.json()
+  const body_string = JSON.stringify(body)
+  const res = await fetch('sas')
+  return new Response(body_string)
+})
