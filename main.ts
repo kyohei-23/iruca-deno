97164c353c60d560cc25897a1dc43c156312938f
KyoheiOrg <tgifz5jmr@gmail.com>
2023-03-03 10:45:28 +0900

    quiet env vars

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-const ROOM_ID = Deno.env.get('ROOM_ID')
-const API_KEY = Deno.env.get('API_KEY')
+// const ROOM_ID = Deno.env.get('ROOM_ID')
+// const API_KEY = Deno.env.get('API_KEY')
 
 
 
