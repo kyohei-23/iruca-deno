bd80fe71c82e4390bed8931b43a2b2258bc0565d
KyoheiOrg <tgifz5jmr@gmail.com>
2023-03-14 15:07:14 +0900

    Deno deployスケジュールのテスト

--- main.ts
+++ main.ts
@@ -1,12 +1,42 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+// import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+// @deno-types="npm:@types/node-schedule"
+import { scheduleJob } from "npm:node-schedule"
 
 // const ROOM_ID = Deno.env.get('ROOM_ID')
 // const API_KEY = Deno.env.get('API_KEY')
 
+// type CalendarEvent = {
+//   title: string;
+//   start: Date;
+//   end: Date;
+// }
 
+// type ReqFromGAS = {
+//   user: string;
+//   events: CalendarEvent[];
+//   str: string;
+// }
 
-serve(async (req)=> {
-  const body = await req.json()
-  const body_string = body.str
-  return new Response(body_string)
+// serve(async (req)=> {
+//   /**
+//    * 識別ヘッダーがない、もしくはbodyがjson形式でない場合のリクエストを受け取ったら403エラーを戻す
+//    * referrerをとるのがベストだと思うが、実行がそれぞれのAppSriptになるため、代わりにrequest headerを利用
+//    */
+//   if(
+//     req.headers.get('gas-iruca-relation') === null ||
+//     req.headers.get('Content-Type') !== 'application/json'
+//   ) return new Response("your request is not allowed", {status: 403})
+
+//   const body = await req.json() as Awaited<Promise<ReqFromGAS>>
+//   const body_string = body.str
+//   for(const event of body.events){
+//     scheduleJob(event.start, async ()=>{
+//       await fetch("")
+//     })
+//   }
+//   return new Response(body_string)
+// })
+
+scheduleJob(new Date('2023-03-14T16:00:00'), ()=>{
+  console.log("scheduled job is fired")
 })
